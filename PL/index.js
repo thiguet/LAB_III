a1931172c7638f1a6919d354df024f75e735dd0b
Thiguet <thiguet2014@gmail.com>
2019-11-23 12:03:34 -0300

    Updating index to js extension
    Updating index.to js extension and also optimizing isPalindrome function

--- PL/index.js
+++ PL/index.js
@@ -1,11 +1,11 @@
 import "./styles.css";
 
 function main(str) {
-  const result = [];
-  let prefix = "";
-  let answer = -1;
+  var result = [];
+  var prefix = "";
+  var answer = -1;
 
-  const permutations = getPermutationsInString(str, prefix, result, str.length);
+  var permutations = getPermutationsInString(str, prefix, result, str.length);
 
   for (var i = 0; i < permutations.length; i++) {
     if (isPalindrome(permutations[i]) === 1) {
@@ -21,6 +21,7 @@ function getPermutationsInString(remainingStr, prefix, result, maxLength) {
   if (remainingStr.length > 2) {
     for (var j = 0; j < remainingStr.length; j++) {
       if (remainingStr.length === maxLength) prefix = "";
+
       var strArrAux = remainingStr.split("");
       var spliced = strArrAux.splice(j, 1);
       var remainingElements = strArrAux.join("");
@@ -47,14 +48,18 @@ function getPermutationsInString(remainingStr, prefix, result, maxLength) {
 
 function isPalindrome(str) {
   var resp = -1;
-  for (var i = 0; i < str.length / 2; i++) {
-    if (str[i] === str[str.length - i - 1]) {
-      resp = 1;
-    } else {
-      return -1;
-    }
+  if (
+    str ===
+    str
+      .split("")
+      .reverse()
+      .join("")
+  ) {
+    resp = 1;
+  } else {
+    return -1;
   }
   return resp;
 }
 
-document.getElementById("app").innerHTML = main("adsadf");
+document.getElementById("app").innerHTML = "<h1>" + main("111111qqgf") + "</h1>";
